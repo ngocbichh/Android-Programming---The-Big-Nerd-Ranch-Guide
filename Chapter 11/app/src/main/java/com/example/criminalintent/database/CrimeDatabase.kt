a790625e6ac297ab391622bf094163cbf91ea3ea
ngocbichh <you@example.com>
2021-07-27 13:14:28 +0700

    Add Challenge: Addressing the Schema Warning

--- Chapter 11/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
+++ Chapter 11/app/src/main/java/com/example/criminalintent/database/CrimeDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.criminalintent.Crime
 
-@Database (entities = [Crime::class], version = 1)
+@Database (entities = [Crime::class], version = 1, exportSchema = false)
 @TypeConverters (CrimeTypeConverters::class)
 
 abstract  class CrimeDatabase : RoomDatabase() {
