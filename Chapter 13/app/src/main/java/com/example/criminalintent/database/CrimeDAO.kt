9c4c13c985e9414fb750c3421bfdcf7681a6fdf7
ngocbichh <you@example.com>
2021-07-27 16:49:14 +0700

    Dialogs

---
+++ Chapter 13/app/src/main/java/com/example/criminalintent/database/CrimeDAO.kt
@@ -0,0 +1,23 @@
+package com.example.criminalintent.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Update
+import com.example.criminalintent.Crime
+import java.util.*
+
+@Dao
+interface CrimeDAO {
+    @Query("SELECT * FROM crime")
+    fun getCrime(): LiveData<List<Crime>>
+
+    @Query("SELECT * FROM crime WHERE id=(:id)")
+    fun getCrime(id: UUID): LiveData<Crime?>
+
+    @Update
+    fun updateCrime(crime: Crime)
+
+    @Update
+    fun addCrime(crime: Crime)
+}
