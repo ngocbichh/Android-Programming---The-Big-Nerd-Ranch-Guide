17f315ded1caf7dfcf7890f39ddd71158647fbb0
ngocbichh <you@example.com>
2021-08-05 17:42:04 +0700

    Add Chapter 26

---
+++ Chapter 26/app/src/main/java/com/bignerdranch/android/photogallery/api/PhotoInterceptor.kt
@@ -0,0 +1,25 @@
+package com.bignerdranch.android.photogallery.api
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+
+private const val API_KEY = "da7cf567683185572dd4120d754edccb"
+class PhotoInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest: Request = chain.request()
+        val newUrl: HttpUrl = originalRequest.url().newBuilder()
+            .addQueryParameter("api_key", API_KEY)
+            .addQueryParameter("format", "json")
+            .addQueryParameter("nojsoncallback", "1")
+            .addQueryParameter("extras", "url_s")
+            .addQueryParameter("safesearch", "1")
+            .build()
+        val newRequest: Request = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+        return chain.proceed(newRequest)
+    }
+}
